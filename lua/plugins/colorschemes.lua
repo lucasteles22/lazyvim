57512eda0d7d77893cb3c426b17ffc4bde6196a9
Lucas Teles <lucasteles22@gmail.com>
2026-04-23 22:26:48 -0300

    add background transparente catppuccin

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,8 +1,28 @@
 return {
   { "ellisonleao/gruvbox.nvim" },
-  { "catppuccin/nvim", name = "catppuccin" },
   { "rose-pine/neovim", name = "rose-pine" },
   { "rebelot/kanagawa.nvim" },
   { "Mofiqul/dracula.nvim" },
   { "shaunsingh/nord.nvim" },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      transparent_background = true,
+      integrations = {
+        snacks = {
+          enabled = true,
+          indent_scope_color = "",
+        },
+      },
+      custom_highlights = function(colors)
+        return {
+          NeoTreeNormal = { bg = "NONE" },
+          NeoTreeNormalNC = { bg = "NONE" },
+          NormalFloat = { bg = "NONE" },
+          FloatBorder = { bg = "NONE" },
+        }
+      end,
+    },
+  },
 }
